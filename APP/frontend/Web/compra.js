c6daba301cc2442dbe86a120d55babd555503efd
Alberto <aandgom@etsinf.upv.es>
2024-05-04 14:02:05 +0200

    cambios

--- APP/frontend/Web/compra.js
+++ APP/frontend/Web/compra.js
@@ -2,16 +2,26 @@
 document.getElementById('buy-cart').addEventListener('click', async () => {
 
     allProducts = allProducts.map(product => {
+        //PARA NO GUARDAR LA IMAGEN EN LA BBDD
         let newProduct = {...product};
         delete newProduct.image;
         return newProduct;
     });
-    
-    const res = await fetch('http://localhost:4000/pedido', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify([allProducts]),
-    });
+
+    try{
+        const res = await fetch('http://localhost:4000/pedido', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify([allProducts]),
+        });
+
+        if(!res.ok) {
+            throw new Error(`HTTP error! status: ${res.status}`);
+        }
+    }
+    catch (err) {
+        console.log("Ha ocurrido un error en la solicitud:", err.message);
+    }
 });
--- APP/frontend/Web/mqtt.js
+++ APP/frontend/Web/mqtt.js
@@ -1,22 +1,7 @@
 const url = "ws://broker.emqx.io:8083/mqtt"
 var client = mqtt.connect(url);
 
-function eventoConectar(){
-    client.subscribe("alberto", function(err){
-        if(!err){
-            client.publish("alberto", "hi there!");
-        }
-    });
-}
-
-function eventoMensaje(topic, message){
-    console.log(message.toString());
-    client.end();
-}
-
-client.on("connect", eventoConectar);
-
-document.getElementById('buy-cart').addEventListener('click', async () => {
+document.getElementById('buy-cart').addEventListener('click', async () =>{
 
     allProducts = allProducts.map(product => {
         //PARA NO GUARDAR LA IMAGEN EN LA BBDD
@@ -25,6 +10,25 @@ document.getElementById('buy-cart').addEventListener('click', async () => {
         return newProduct;
     });
 
-    client.on("connect", eventoConectar);
-    client.on("message", eventoMensaje);
+    client.on('connect', function () {
+        client.subscribe('alberto', function (err) {
+            if (!err) {
+                client.publish('alberto', 'juana', function(err) {
+                    if(err) {
+                        console.log("Error al publicar el mensaje", err);
+                    }
+                });
+            } 
+            else{
+                console.log("Error al suscribirse al topic", err);
+            }
+        });
+    });
+    
+    client.on('error', function (err) {
+        console.log('Error de conexión MQTT:', err);
+    });
+    //client.on('connect', eventoConectar);
+    //client.on('message', eventoMensaje);
 });
+
