325122cad2eb3527cf40aafa551cfe39e27ffe40
Alberto <aandgom@etsinf.upv.es>
2024-05-08 17:55:24 +0200

    su

--- APP/frontend/Web/cartService.js
+++
@@ -1,8 +0,0 @@
-function agregarCarrito(producto){
-    const memoria = JSON.parse(localStorage.getItem("cajas"));
-    if(!memoria){
-        const nuevoProducto = producto;
-        nuevoProducto.cantidad = 1;
-        localStorage.setItem("cajas", JSON.stringify([nuevoProducto]));
-    }
-}
